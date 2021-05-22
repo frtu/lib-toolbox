a6255b2743bbadb7c98071b96ce4e2ce9c397114
Tu, Fred <frederic.tu@gmail.com>
2021-05-23 00:37:06 +0800

    doc(ut) : Add structured UT

--- java/lib-mail/src/test/java/com/github/frtu/mail/MailServiceTest.java
+++ java/lib-mail/src/test/java/com/github/frtu/mail/MailServiceTest.java
@@ -11,17 +11,35 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class MailServiceTest {
     @Test
     void splitEmailSingleEmail() {
+        //--------------------------------------
+        // 1. Prepare data & Init service
+        //--------------------------------------
         final MailService mailService = new MailService();
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
         final String[] emails = mailService.splitEmail("xxx@yyy.com");
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
         Arrays.stream(emails).forEach(email -> LOGGER.trace(email));
         assertArrayEquals(new String[]{"xxx@yyy.com"}, emails);
     }
 
     @Test
     void splitEmailArray() {
+        //--------------------------------------
+        // 1. Prepare data & Init service
+        //--------------------------------------
         final MailService mailService = new MailService();
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
         final String[] emails = mailService.splitEmail("xxx@yyy.com,zzz@yyy.com,aaa@yyy.com");
         Arrays.stream(emails).forEach(email -> LOGGER.trace(email));
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
         assertArrayEquals(new String[]{"xxx@yyy.com", "zzz@yyy.com", "aaa@yyy.com"}, emails);
     }
 }
