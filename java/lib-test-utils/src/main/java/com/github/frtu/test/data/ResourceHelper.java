cd8f98b595f62d465bf1465351922311ea97bc32
Tu, Fred <frederic.tu@gmail.com>
2021-08-21 23:34:21 +0800

    Make it compatible with java 8 & fix encoding

--- java/lib-test-utils/src/main/java/com/github/frtu/test/data/ResourceHelper.java
+++ java/lib-test-utils/src/main/java/com/github/frtu/test/data/ResourceHelper.java
@@ -6,10 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Allow to easily read data from resources
@@ -39,7 +37,7 @@ public class ResourceHelper {
 
     public String readFromFile(File file) {
         StringBuilder contentBuilder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
             String sCurrentLine;
             while ((sCurrentLine = br.readLine()) != null) {
                 contentBuilder.append(sCurrentLine).append("\n");
