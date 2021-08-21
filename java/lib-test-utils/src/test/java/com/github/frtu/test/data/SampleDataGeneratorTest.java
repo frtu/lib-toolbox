5a08cf931b13aa01d9935dab73c73ef00e9a6dd3
Tu, Fred <frederic.tu@gmail.com>
2021-08-21 23:21:37 +0800

    Migrate to Junit5

--- java/lib-test-utils/src/test/java/com/github/frtu/test/data/SampleDataGeneratorTest.java
+++ java/lib-test-utils/src/test/java/com/github/frtu/test/data/SampleDataGeneratorTest.java
@@ -2,8 +2,8 @@ package com.github.frtu.test.data;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Frédéric TU
