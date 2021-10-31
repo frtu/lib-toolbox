089a8432bba037da097bfa5464726b83e4b2972b
Tu, Fred <frederic.tu@gmail.com>
2021-10-31 22:06:12 +0800

    Adding ResourceController to serve resources

---
+++ kotlin/samples/sample-reactive-web/src/main/kotlin/com/github/frtu/sample/reactive/web/controller/ResourceController.kt
@@ -0,0 +1,27 @@
+package com.github.frtu.sample.reactive.web.controller
+
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.core.io.buffer.DefaultDataBufferFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+/**
+ * Mock a PDF service
+ */
+@RestController
+@RequestMapping("/resource")
+class ResourceController {
+    @GetMapping("/pdf")
+    fun generatePdf(): Flux<DataBuffer> {
+        val resource = DefaultResourceLoader().getResource("classpath:kotlin_docs.pdf")
+        return DataBufferUtils.read(
+            resource,
+            DefaultDataBufferFactory(),
+            4096
+        )
+    }
+}
