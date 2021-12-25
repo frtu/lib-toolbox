7c9108b87872771256169a28dc1b5ecbe343249c
Tu, Fred <frederic.tu@gmail.com>
2021-12-26 00:10:34 +0800

    spring sample : allow namespace declaration from UT

--- kotlin/lib-workflow/src/test/kotlin/com/github/frtu/kotlin/patterns/spring/SpringConfig.kt
+++ kotlin/lib-workflow/src/test/kotlin/com/github/frtu/kotlin/patterns/spring/SpringConfig.kt
@@ -1,9 +1,15 @@
 package com.github.frtu.kotlin.patterns.spring
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ComponentScan
 class SpringConfig {
+    @Bean(value = ["${SpringFlowRegistry.SPRING_NAMESPACE + GoodSampleFlow.FLOW_NAME}"])
+    fun goodSampleFlow() = GoodSampleFlow()
+
+    @Bean("${SpringFlowRegistry.SPRING_NAMESPACE + ErrorSampleFlow.FLOW_NAME}")
+    fun errorSampleFlow() = ErrorSampleFlow()
 }
