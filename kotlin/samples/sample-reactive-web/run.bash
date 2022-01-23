bb8b1808a226772c3780c5eb1a81b9df5b200874
Tu, Fred <frederic.tu@gmail.com>
2022-01-23 16:43:59 +0800

    Reorganize k8s maven script & add into run.bash

--- kotlin/samples/sample-reactive-web/run.bash
+++ kotlin/samples/sample-reactive-web/run.bash
@@ -3,11 +3,18 @@ sample-reactive-webbuild() {
   echo "mvn clean package"
   mvn clean package
 }
+echo "-> Run bash individually - Type 'sample-reactive-k8s' to run the executable"
+sample-reactive-k8s() {
+  echo "Running executable > mvn k8s:resource k8s:build"
+  mvn k8s:resource k8s:build
+}
+echo "-> Run bash individually - Type '$sample-reactive-registryk8s' to run the executable"
+sample-reactive-k8sregistry() {
+  echo "Running executable > mvn k8s:resource k8s:build k8s:push -Pregistry-k8s"
+  mvn k8s:resource k8s:build k8s:push -Pregistry-k8s
+}
 echo "-> Run bash individually - Type 'sample-reactive-webrun' to run the executable"
 sample-reactive-webrun() {
   echo "Running executable > mvn spring-boot:run"
   mvn spring-boot:run
 }
-
-sample-reactive-webbuild
-sample-reactive-webrun
