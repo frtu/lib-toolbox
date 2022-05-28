efb9b5addd2f1f8651d6c2231d0f0d2f74bc6b7a
Tu, Fred <frederic.tu@gmail.com>
2022-05-28 15:58:05 +0800

    lib-serdes-protobuf: enhance Timestamp <-> java.time conversion

--- kotlin/lib-serdes-protobuf/src/main/kotlin/com/github/frtu/kotlin/protobuf/utils/TimestampExt.kt
+++ kotlin/lib-serdes-protobuf/src/main/kotlin/com/github/frtu/kotlin/protobuf/utils/TimestampExt.kt
@@ -2,9 +2,16 @@ package com.github.frtu.kotlin.protobuf.utils
 
 import com.google.protobuf.Timestamp
 import com.google.protobuf.util.Timestamps
-import java.time.Instant
+import java.time.*
 
 fun Long.toTimestamp(): Timestamp = Timestamps.fromMillis(this)
+
 fun Instant.toTimestamp(): Timestamp = this.toEpochMilli().toTimestamp()
+fun ZonedDateTime.toTimestamp(): Timestamp = this.toInstant().toTimestamp()
+fun LocalDateTime.toTimestamp(offset: ZoneOffset = ZoneOffset.UTC): Timestamp = this.toInstant(offset).toTimestamp()
 
 fun Timestamp.toEpochMilli(): Long = Timestamps.toMillis(this)
+fun Timestamp.toInstant(): Instant = Instant.ofEpochSecond(this.seconds, this.nanos.toLong())
+fun Timestamp.toZonedDateTime(zoneId: ZoneId = ZoneId.of("UTC")): ZonedDateTime = toInstant().atZone(zoneId)
+fun Timestamp.toLocalDateTime(offset: ZoneOffset = ZoneOffset.UTC): LocalDateTime =
+    LocalDateTime.ofInstant(toInstant(), offset)
