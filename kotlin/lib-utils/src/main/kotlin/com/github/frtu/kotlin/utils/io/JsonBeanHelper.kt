f70afaf1b591ce8c7d40e248a20aee215c9ff326
Tu, Fred <frederic.tu@gmail.com>
2022-07-16 12:21:34 +0800

    feat: move Json utility into lib-utils

--- kotlin/lib-utils/src/main/kotlin/com/github/frtu/kotlin/utils/io/JsonBeanHelper.kt
+++ kotlin/lib-utils/src/main/kotlin/com/github/frtu/kotlin/utils/io/JsonBeanHelper.kt
@@ -1,7 +1,10 @@
 package com.github.frtu.kotlin.utils.io
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.frtu.kotlin.utils.io.JsonUtil.jsonToBean
 
 /**
  * Allow to easily deserialize an object from external JSON source
@@ -17,14 +20,27 @@ class JsonBeanHelper<T>(
      * Allow to easily deserialize an object using a class and a JSON file location
      * using the clazz from constructor
      */
-    fun toBean(location: String): T? = toBean(clazz!!, location)
+    fun toBean(location: String): T? = toBean(location, clazz!!)
 
     /**
      * Allow to easily deserialize an object using a class and a JSON file location.
      */
-    fun <T> toBean(clazz: Class<T>, location: String): T? {
+    fun <T> toBean(location: String, clazz: Class<T>): T? {
         logger.debug("Deserialize class:${clazz} from location:${location}")
         val resource = resourceLoader.getResource(location)
         return mapper.readValue(resource.file, clazz)
     }
+
+    fun <T> jsonToBean(payload: String, clazz: Class<T>): T = mapper.readValue(payload, clazz)
 }
+
+object JsonUtil {
+    fun <T> jsonToBean(payload: String, clazz: Class<T>): T = jsonBeanHelper.jsonToBean(payload, clazz)
+
+    private val jsonBeanHelper =
+        JsonBeanHelper<Any>(jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL))
+}
+
+fun String.toJsonNode() = toJsonObj(JsonNode::class.java)
+
+fun <T> String.toJsonObj(clazz: Class<T>) = jsonToBean(this, clazz)
--- kotlin/lib-utils/src/test/kotlin/com/github/frtu/kotlin/utils/io/JsonBeanHelperTest.kt
+++ kotlin/lib-utils/src/test/kotlin/com/github/frtu/kotlin/utils/io/JsonBeanHelperTest.kt
@@ -9,13 +9,19 @@ internal class JsonBeanHelperTest {
 
     @Test
     fun `Testing beanManager toBean`() {
-        val dummyBean = beanManager.toBean(DummyBean::class.java, "classpath:dummy-bean.json")!!
+        val dummyBean = beanManager.toBean("classpath:dummy-bean.json", DummyBean::class.java)!!
         //--------------------------------------
         // Validate
         //--------------------------------------
         logger.debug("Found bean:${dummyBean}")
         assertThat(dummyBean.name).isEqualTo("bean_name")
         assertThat(dummyBean.value).isEqualTo("bean_value")
+    }
+
+    @Test
+    fun jsonToObj() {
+        val jsonNode = """{"key": "value"}""".toJsonNode()
+        assertThat(jsonNode["key"].textValue()).isEqualTo("value")
     }
 
     private val logger = LoggerFactory.getLogger(this::class.java)
