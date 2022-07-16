38e3c2dd234c9f0cbe6eaec6f94b7c62d948c4d7
Tu, Fred <frederic.tu@gmail.com>
2022-07-16 23:30:51 +0800

    feat: adding conditional P99

--- kotlin/samples/sample-grpc/README.md
+++ kotlin/samples/sample-grpc/README.md
@@ -6,6 +6,64 @@ Sample gRPC using spring-boot & R2DBC :
 
 * R2DBC Coroutine repository : ```com.github.frtu.sample.persistence```
 * gRPC server-client implementation : ```com.github.frtu.sample.rpc.grpc```
+* Adding all metrics
+
+## Metrics
+
+### gRPC
+
+#### Throughput IN / OUT 
+
+```
+# HELP grpc_server_requests_received_messages_total The total number of requests received (counter)
+grpc_server_requests_received_messages_total
+# HELP grpc_server_responses_sent_messages_total The total number of responses sent (counter)
+grpc_server_responses_sent_messages_total
+```
+
+#### Duration
+
+```
+# HELP grpc_server_processing_duration_seconds The total time taken for the server to complete the call (summary)
+grpc_server_processing_duration_seconds_count
+grpc_server_processing_duration_seconds_sum
+# HELP grpc_server_processing_duration_seconds_max The total time taken for the server to complete the call (gauge)
+grpc_server_processing_duration_seconds_max
+```
+
+### Database
+
+#### Spring Data Repository
+
+```
+# HELP spring_data_repository_invocations_seconds_max (gauge)
+spring_data_repository_invocations_seconds_max
+# HELP spring_data_repository_invocations_seconds (summary)
+spring_data_repository_invocations_seconds_count
+spring_data_repository_invocations_seconds_sum
+```
+
+* exception="None",
+* method="save",
+* repository="IEmailRepository",
+* state="SUCCESS" OR state="CANCELED"
+
+#### R2DBC
+
+```
+# HELP r2dbc_pool_acquired_connections Size of successfully acquired connections which are in active use. (gauge)
+r2dbc_pool_acquired_connections{name="connectionFactory",} 0.0
+# HELP r2dbc_pool_max_allocated_connections Maximum size of allocated connections that this pool allows. (gauge)
+r2dbc_pool_max_allocated_connections{name="connectionFactory",} 10.0
+# HELP r2dbc_pool_idle_connections Size of idle connections in the pool. (gauge)
+r2dbc_pool_idle_connections{name="connectionFactory",} 10.0
+# HELP r2dbc_pool_allocated_connections Size of allocated connections in the pool which are in active use or idle. (gauge)
+r2dbc_pool_allocated_connections{name="connectionFactory",} 10.0
+# HELP r2dbc_pool_pending_connections Size of pending to acquire connections from the underlying connection factory. (gauge)
+r2dbc_pool_pending_connections{name="connectionFactory",} 0.0
+# HELP r2dbc_pool_max_pending_connections Maximum size of pending state to acquire connections that this pool allows. (gauge)
+r2dbc_pool_max_pending_connections{name="connectionFactory",} 2.147483647E9
+```
 
 ## See also
 
--- kotlin/samples/sample-grpc/src/main/kotlin/com/github/frtu/sample/config/ObservabilityConfig.kt
+++ kotlin/samples/sample-grpc/src/main/kotlin/com/github/frtu/sample/config/ObservabilityConfig.kt
@@ -1,10 +1,17 @@
 package com.github.frtu.sample.config
 
+import io.micrometer.core.instrument.Meter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Metrics
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry
+import io.micrometer.core.instrument.config.MeterFilter
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig
+import java.time.Duration
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor
 import net.devh.boot.grpc.server.metric.MetricCollectingServerInterceptor
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -17,4 +24,43 @@ class ObservabilityConfig {
 
     @GrpcGlobalServerInterceptor
     fun serverLogGrpcInterceptor(meterRegistry: MeterRegistry) = MetricCollectingServerInterceptor(meterRegistry)
+
+    @Value("\${spring.application.name:application-name}")
+    lateinit var applicationName: String
+
+    @Bean
+    fun metricsCommonTags(meterFilter: MeterFilter): MeterRegistryCustomizer<MeterRegistry> =
+        MeterRegistryCustomizer { registry: MeterRegistry ->
+            // Aligned with https://grafana.com/grafana/dashboards/4701
+            logger.info("METRICS - Adding application='{}' to MeterRegistry", applicationName)
+            registry.config()
+                .commonTags("application", applicationName)
+                .meterFilter(meterFilter)
+        }
+
+    @Bean
+    fun meterFilter() = object : MeterFilter {
+        override fun configure(id: Meter.Id, config: DistributionStatisticConfig): DistributionStatisticConfig =
+            if (id.name.startsWith("grpc.server.processing") // Adding P99 for gRPC
+                || id.name.startsWith("spring.data.repository") // Adding P99 for Repository
+            ) {
+                logger.debug("METRICS - Id:${id} -> adding P99")
+                DistributionStatisticConfig.builder()
+                    .percentilesHistogram(true)
+                    .percentiles(0.99)
+                    .sla(
+                        Duration.ofMillis(50).toNanos(),
+                        Duration.ofMillis(200).toNanos(),
+                        Duration.ofSeconds(1).toNanos()
+                    )
+                    .minimumExpectedValue(50.0)
+                    .maximumExpectedValue(500.0)
+                    .build()
+                    .merge(config)
+            } else {
+                config
+            }
+    }
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
 }
