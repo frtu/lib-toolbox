3936f6d2753584ea384fbca14ddaca1ba2368f8c
Tu, Fred <frederic.tu@gmail.com>
2022-08-22 00:30:18 +0800

    feat: sample-serverless simplify state transition & add run actions

--- kotlin/samples/sample-serverless/src/test/kotlin/com/github/frtu/sample/serverless/workflow/ServerlessWorkflowMain.kt
+++ kotlin/samples/sample-serverless/src/test/kotlin/com/github/frtu/sample/serverless/workflow/ServerlessWorkflowMain.kt
@@ -3,6 +3,7 @@ package com.github.frtu.sample.serverless.workflow
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.frtu.kotlin.utils.io.ResourceHelper
+import io.serverlessworkflow.api.actions.Action
 import io.serverlessworkflow.api.interfaces.State
 import io.serverlessworkflow.api.states.EventState
 import io.serverlessworkflow.api.states.OperationState
@@ -10,7 +11,6 @@ import io.serverlessworkflow.api.states.SleepState
 import io.serverlessworkflow.api.states.SwitchState
 import io.serverlessworkflow.api.transitions.Transition
 import io.serverlessworkflow.diagram.WorkflowDiagramImpl
-import java.io.File
 import java.time.Duration
 import io.serverlessworkflow.api.Workflow as ServerlessWorkflow
 
@@ -24,8 +24,8 @@ fun main() {
     serverlessWorkflow.assertValidity()
     printJson(serverlessWorkflow)
 
-    val file = File("${serverlessWorkflow.name}.svg")
-    file.writeText(generateSvg(serverlessWorkflow))
+//    val file = File("${serverlessWorkflow.name}.svg")
+//    file.writeText(generateSvg(serverlessWorkflow))
 
     traverseState(serverlessWorkflow.getState(), serverlessWorkflow)
 }
@@ -46,67 +46,62 @@ fun executeStateAndReturnNext(workflowState: State, serverlessWorkflow: Serverle
                 // TODO support more than 1 event (AND or OR) depending on eventState.isExclusive
                 val firstEvent = eventState.onEvents[0]
 
-                val eventStateActions = firstEvent.actions
-
-                for (action in eventStateActions) {
-                    if (action.sleep != null && action.sleep.before != null) {
-                        sleep(Duration.parse(action.sleep.before))
-                    }
-                    println("execute(${action.functionRef.refName})")
-                    if (action.sleep != null && action.sleep.after != null) {
-                        sleep(Duration.parse(action.sleep.after))
-                    }
-                }
+                run(firstEvent.actions)
             }
-            eventState.transition?.nextState
-                ?.let { serverlessWorkflow.getStateWithName(it) }
-                ?: null
+            eventState.transition
         }
         is OperationState -> {
             val operationState = workflowState
-            if (!operationState.actions.isNullOrEmpty()) {
-                for (action in operationState.actions) {
-                    if (action.sleep != null && action.sleep.before != null) {
-                        sleep(Duration.parse(action.sleep.before))
-                    }
-                    println("execute(${action.functionRef.refName})")
-                    if (action.sleep != null && action.sleep.after != null) {
-                        sleep(Duration.parse(action.sleep.after))
-                    }
-                }
-            }
-            operationState.transition?.nextState
-                ?.let { serverlessWorkflow.getStateWithName(it) }
-                ?: null
+            run(operationState.actions)
+            operationState.transition
         }
         is SwitchState -> {
             val switchState = workflowState
 
             val transition: Transition = switchState.dataConditions.firstOrNull {
-                println("execute(${it.condition})")
+                println("filter(${it.condition})")
                 true
             }?.let {
                 it.transition
             } ?: switchState.defaultCondition.transition
 
-            transition.nextState
-                ?.let { serverlessWorkflow.getStateWithName(it) }
-                ?: null
+            transition
         }
         is SleepState -> {
             val sleepState = workflowState
-            sleepState.duration?.let {
-                sleep(Duration.parse(it))
-            }
-            sleepState.transition?.nextState
-                ?.let { serverlessWorkflow.getStateWithName(it) }
-                ?: null
+            sleepState.duration?.let { sleep(it) }
+            sleepState.transition
         }
         else -> {
             println("Invalid or unsupported in demo dsl workflow state: $workflowState")
             null
         }
+    }?.nextState
+        ?.let { serverlessWorkflow.getStateWithName(it) }
+        ?: null
+
+
+fun run(actions: List<Action>) {
+    if (!actions.isNullOrEmpty()) {
+        for (action in actions) {
+            run(action)
+        }
     }
+}
+
+fun run(action: Action) {
+    if (action.sleep != null && action.sleep.before != null) {
+        sleep(action.sleep.before)
+    }
+    println("${action.functionRef.refName}(${action.functionRef.arguments})")
+    if (action.sleep != null && action.sleep.after != null) {
+        sleep(action.sleep.after)
+    }
+}
+
+fun sleep(text: CharSequence) {
+    sleep(Duration.parse(text))
+}
 
 fun sleep(duration: Duration) {
     Thread.sleep(duration.toMillis())
