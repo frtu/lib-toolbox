0c470972a99b847847e8d4c4cc87f7a3a32e72a0
Tu, Fred <frederic.tu@gmail.com>
2024-10-20 23:31:18 +0800

    feat: lib-spring-boot-llm-os - adding unboxed version of Answer info

--- kotlin/lib-spring-boot-llm-os/src/main/kotlin/com/github/frtu/kotlin/llm/os/llm/model/Answer.kt
+++ kotlin/lib-spring-boot-llm-os/src/main/kotlin/com/github/frtu/kotlin/llm/os/llm/model/Answer.kt
@@ -2,16 +2,48 @@ package com.github.frtu.kotlin.llm.os.llm.model
 
 import com.aallam.openai.api.chat.ChatChoice
 import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ToolCall
+import com.aallam.openai.api.core.FinishReason
 
 /**
  * Response message returned from a chat
  */
 data class Answer(private val chatChoice: ChatChoice) {
+    /**
+     * Returning message content
+     */
+    val content: String?
+        get() = message.content
+
+    /**
+     * Returning list of tool calls
+     */
+    val toolCalls: List<ToolCall>?
+        get() = message.toolCalls
+
+    /**
+     * If answer return tool calls
+     */
+    val hasToolCall: Boolean
+        get() = !toolCalls.isNullOrEmpty()
+
+    /**
+     * Returning raw `ChatMessage`
+     */
     val message: ChatMessage
         get() = chatChoice.message
 
-    val content: String?
-        get() = chatChoice.message.content
+    /**
+     * Returning the `FinishReason`
+     */
+    val finishReason: FinishReason?
+        get() = chatChoice.finishReason
+
+    /**
+     * Internal `ChatChoice` index
+     */
+    val index: Int
+        get() = chatChoice.index
 
     val invokeFunction: InvokeFunction?
         get() = with(chatChoice.message) {
